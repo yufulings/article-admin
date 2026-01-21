ec52049787184cf10b07883c154cfa1facb0ad97
envyafish <orekiii234@gmail.com>
2026-01-21 15:13:40 +0800

    coding

--- src/features/settings/index.tsx
+++ src/features/settings/index.tsx
@@ -1,19 +1,12 @@
-import { Outlet } from '@tanstack/react-router';
-import {
-  Bell,
-  Download,
-  Folder,
-  UserPen,
-  Settings2,
-} from 'lucide-react'
-import { ConfigDrawer } from '@/components/config-drawer';
-import { Header } from '@/components/layout/header';
-import { Main } from '@/components/layout/main';
-import { Search } from '@/components/search';
-import { ThemeSwitch } from '@/components/theme-switch';
-import { TopNav } from '@/features/settings/components/top-nav.tsx';
+import { Outlet } from '@tanstack/react-router'
+import { Bell, Download, UserPen, Settings2, Route } from 'lucide-react'
+import { ConfigDrawer } from '@/components/config-drawer'
 import { ImageModeSwitch } from '@/components/image-mode-switch.tsx'
-
+import { Header } from '@/components/layout/header'
+import { Main } from '@/components/layout/main'
+import { Search } from '@/components/search'
+import { ThemeSwitch } from '@/components/theme-switch'
+import { TopNav } from '@/features/settings/components/top-nav.tsx'
 
 const sidebarNavItems = [
   {
@@ -22,9 +15,9 @@ const sidebarNavItems = [
     icon: <UserPen size={18} />,
   },
   {
-    title: '目录',
+    title: '路由',
     href: '/settings/folder',
-    icon: <Folder size={18} />,
+    icon: <Route size={18} />,
   },
   {
     title: '下载器',
@@ -35,7 +28,7 @@ const sidebarNavItems = [
     title: '通知',
     href: '/settings/notifications',
     icon: <Bell size={18} />,
-  }
+  },
 ]
 
 export function Settings() {
@@ -45,25 +38,25 @@ export function Settings() {
       <Header fixed>
         <Search />
         <div className='ms-auto flex items-center space-x-4'>
-          <ImageModeSwitch/>
+          <ImageModeSwitch />
           <ThemeSwitch />
           <ConfigDrawer />
         </div>
       </Header>
 
       <Main className='flex h-[calc(100vh-4rem)] flex-col'>
-        <div className="mb-8">
-          <div className="flex items-center gap-3 mb-2">
-            <Settings2 className="h-8 w-8 text-primary" />
-            <h1 className="text-3xl font-bold">服务器配置</h1>
+        <div className='mb-8'>
+          <div className='mb-2 flex items-center gap-3'>
+            <Settings2 className='h-8 w-8 text-primary' />
+            <h1 className='text-3xl font-bold'>服务器配置</h1>
           </div>
-          <p className="text-muted-foreground">
+          <p className='text-muted-foreground'>
             配置和管理您的下载器、通知渠道等
           </p>
         </div>
         <TopNav items={sidebarNavItems} />
-        <div className="flex-1 overflow-y-auto">
-          <Outlet/>
+        <div className='flex-1 overflow-y-auto'>
+          <Outlet />
         </div>
       </Main>
     </>
